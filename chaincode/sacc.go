b217d7ba2b4a02b01238950f638d4975ff66adb5
Guik <rndlr96@gmail.com>
2018-11-28 11:02:10 +0900

    update get, set function

--- chaincode/sacc.go
+++ chaincode/sacc.go
@@ -24,3 +24,61 @@ func (t *SimpleAsset) init(stub shim.ChaincodeStubInterface) peer.Response {
   }
   return shim.Success(nil)
 }
+
+func (t *SimpleAsset) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+
+  function, args := stub.GetFunctionAndParameters()
+
+  var result string
+  var err error
+
+  if function == "set" {
+    result, err := set(stub, args)
+  } else {
+    result, err := get(stub, args)
+  }
+
+  if err != nil {
+    return shim.Error(err.Error())
+  }
+
+  return shim.Success([]byte(result))
+}
+
+func set(stub shim.ChaincodeStubInterface, args []string) (string, error) {
+  if len(args) != 2 {
+    return "", fmt.Errorf("Incorrect arguments. Expecting a key and a value")
+  }
+
+  err := stub.PutState(args[0], []byte(args[1]))
+
+  if err != nil {
+    return "", fmt.Errorf("Failed to set asset: %s", args[0])
+  }
+
+  return args[1], nil
+}
+
+func get(stub shim.ChaincodeStubInterface, args []string) (string, error) {
+  if len(args) != 1 {
+    return "", fmt.Errorf("Incorrect arguments. Expecting a key")
+  }
+
+  value, err := stub.GetState(args[0])
+
+  if err != nil {
+    return "", fmt.Errorf("Failed to get asset: %s with error: %s", args[0], err)
+  }
+
+  if value == nil {
+    return "", fmt.Errorf("Asset not found: %s", args[0])
+  }
+
+  return string(value), nil
+}
+
+func main() {
+    if err := shim.Start(new(SimpleAsset)); err != nil {
+            fmt.Printf("Error starting SimpleAsset chaincode: %s", err)
+    }
+}
